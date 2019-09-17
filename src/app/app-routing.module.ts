d98bdc8bbd3012e7dcc8de4a3612b46a1f5b1fdb
Faruk <faruk.tulumcu@ennova-research.com>
2019-09-17 17:16:30 +0200

    last version generic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { OrdersListComponent } from './components/orders/orders-list/orders-list.component';
 import { ProductsListComponent } from './components/products/products-list/products-list.component';
 import { GenericListComponent } from './components/generic/generic-list/generic-list.component';
 
 
 const routes: Routes = [
-  {
-    path: 'orders',
-    component: OrdersListComponent
-  },
-  {
-    path: 'products',
-    component: ProductsListComponent
-  },
   {
     path: 'generic/:name',
     component: GenericListComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,11 @@ import { AppComponent } from './app.component';
 import {ButtonModule} from 'primeng/button';
 import { SharedModule } from './modules/shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
-import { OrdersListComponent } from './components/orders/orders-list/orders-list.component';
-import { ProductsListComponent } from './components/products/products-list/products-list.component';
 import { GenericListComponent } from './components/generic/generic-list/generic-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OrdersListComponent,
-    ProductsListComponent,
     GenericListComponent,
     
   ],
